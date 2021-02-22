78f59911b9f9f440b563b207b7a78d9a1771e6fc
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2021-02-21 20:01:14 -0500

    final commit for now: just hiding form

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import homeHeroTop from "./assets/bg-1.jpg";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Typography } from "@material-ui/core";
 import logoImage from "./assets/logo.svg";
-import { SignupForm } from "./components/Form";
+// import { SignupForm } from "./components/Form";
 import { Fragment } from "react";
 
 const useStyles = makeStyles((theme) => ({
-  homeHero: {
+  homeHeroSection: {
     display: "flex",
     minHeight: "100vh",
     [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
     },
   },
-  homeHeroTextSection: {
+  homeHeroTextArea: {
     minWidth: "400px",
     flex: "50%",
     display: "flex",
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
   homeHeroTextHighlighted: {
     color: "#5850ea",
   },
-  homeHeroImageSection: {
+  homeHeroImageArea: {
     flex: "50%",
   },
   homeHeroImage: {
@@ -47,6 +47,21 @@ const useStyles = makeStyles((theme) => ({
     background: "#5850ea",
     textTransform: "capitalize",
   },
+
+  homeFormSection: {
+    display: "flex",
+    minHeight: "100vh",
+    background: "#25303f",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  homeFormTextArea: {
+    flex: "50%",
+  },
+  homeFormArea: {
+    flex: "50%",
+  },
 }));
 
 function App() {
@@ -54,24 +69,29 @@ function App() {
 
   return (
     <Fragment>
-      <section className={classes.homeHero}>
-        <div className={classes.homeHeroTextSection}>
+      <section className={classes.homeHeroSection}>
+        <div className={classes.homeHeroTextArea}>
           <img src={logoImage} alt='logo' className={classes.logo} />
           <Typography variant='h3' className={classes.homeHeroText}>
-            Data to enrich your{" "}
+            Available in the{" "}
             <span className={classes.homeHeroTextHighlighted}>
-              online business
+              GTA & Scarborough Area
             </span>
           </Typography>
-          <p>Lorem Ipsum decorum, Lorem Ipsum decorum, Lorem Ipsum decorum</p>
+          <p>
+            Contact us today to get a quote without having to wait forever.
+            Construction & services you can count on.
+          </p>
           <Button
             variant='contained'
             color='primary'
-            className={classes.homeHeroButton}>
+            className={classes.homeHeroButton}
+            href='mailto:dimasheconstruction@gmail.com?subject=Service%20Query&body=Description'
+            rel='noreferrer'>
             Contact Us
           </Button>
         </div>
-        <div className={classes.homeHeroImageSection}>
+        <div className={classes.homeHeroImageArea}>
           <img
             src={homeHeroTop}
             alt='home-hero'
@@ -80,9 +100,12 @@ function App() {
         </div>
       </section>
 
-      <section>
-        <SignupForm />
-      </section>
+      {/*<section className={classes.homeFormSection}>
+        <div className={classes.homeFormTextArea}>
+          <SignupForm />
+        </div>
+        <div className={classes.homeFormArea}>Some text here</div>
+  </section>*/}
     </Fragment>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,11 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { Typography } from "@material-ui/core";
 
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  lastName: Yup.string()
+  name: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
@@ -15,33 +13,66 @@ const SignupSchema = Yup.object().shape({
   body: Yup.string().required("Required"),
 });
 
-export const SignupForm = () => (
-  <div>
-    <h1>Signup</h1>
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: "",
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values) => {
-        // same shape as initial values
-        console.log(values);
-      }}>
-      {({ errors, touched }) => (
-        <Form>
-          <Field name='firstName' />
-          <ErrorMessage name='firstName' component='div' />
-          <Field name='lastName' />
-          <ErrorMessage name='lastName' component='div' />
-          <Field name='email' type='email' />
-          <ErrorMessage name='email' component='div' />
-          <Field as='textarea' name='body' />
-          <ErrorMessage name='body' component='div' />
-          <button type='submit'>Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    marginTop: "1.5rem",
+    marginLeft: "1.5rem",
+    marginRight: "1.5rem",
+  },
+  formTitle: {
+    color: "#ffffff",
+    marginBottom: "1.5rem",
+  },
+  form: {
+    background: "#ffffff",
+    borderRadius: "4px",
+    padding: "1rem",
+    display: "flex",
+    flexDirection: "column",
+  },
+  formLabel: {
+    marginBottom: ".5rem",
+  },
+  formInput: {
+    marginBottom: "1rem",
+  },
+  formTextArea: { marginBottom: "1rem" },
+}));
+
+export const SignupForm = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.formContainer}>
+      <Typography variant='h5' className={classes.formTitle}>
+        Contact Us
+      </Typography>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          body: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          // same shape as initial values
+          console.log(values);
+        }}>
+        {({ errors, touched }) => (
+          <Form className={classes.form}>
+            <label className={classes.formLabel}>Name *</label>
+            <Field name='name' className={classes.formInput} />
+            <ErrorMessage name='name' component='div' />
+            <label className={classes.formLabel}>Email *</label>
+            <Field name='email' type='email' className={classes.formInput} />
+            <ErrorMessage name='email' component='div' />
+            <label className={classes.formLabel}>Your message</label>
+            <Field as='textarea' name='body' className={classes.formTextArea} />
+            <ErrorMessage name='body' component='div' />
+            <button type='submit'>Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
